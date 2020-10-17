3a6a865969cf0209d31c1a366068a01339f46007
Dadamuga Mazino <destructicus_lives@yahoo.com>
2020-10-17 16:56:50 +0100

    added light status syncing for desk lights

--- assets/js/app/smarthaus/lights.js
+++ assets/js/app/smarthaus/lights.js
@@ -1,36 +1,54 @@
+const LIGHTS_API = "http://zeenotheinventor.ngrok.io/lights";
+function turnOnLights() {
+    axios
+        .get(`${LIGHTS_API}/on`)
+        .then(function (response) {
+            // handle success
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+}
+
+function turnOffLights() {
+    axios
+        .get(`${LIGHTS_API}/off`)
+        .then(function (response) {
+            // handle success
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+}
+
+async function getLightStatus() {
+
+    try {
+        const status = await axios.get(`${LIGHTS_API}/status`);
+
+        return status.data;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 (function ($) {
-  const LIGHTS_API = "http://zeenotheinventor.ngrok.io/lights";
-  function turnOnLights() {
-    axios
-      .get(`${LIGHTS_API}/on`)
-      .then(function (response) {
-        // handle success
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
-  }
 
-  function turnOffLights() {
-    axios
-      .get(`${LIGHTS_API}/off`)
-      .then(function (response) {
-        // handle success
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
-  }
-
-  $(function () {
-    $("#desk-light-switch").on("click", function (event) {
-      if (event.target.checked) {
-        turnOnLights();
-      } else {
-        turnOffLights();
-      }
+    $(function () {
+        getLightStatus().then((status) => {
+            $("#desk-light-switch").prop('checked', status);
+        });
     });
-  });
+
+    $(function () {
+        $("#desk-light-switch").on("click", function (event) {
+            if (event.target.checked) {
+                turnOnLights();
+            } else {
+                turnOffLights();
+            }
+        });
+    });
 })(jQuery);
